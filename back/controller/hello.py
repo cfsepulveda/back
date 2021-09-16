34db40004830fbc3a5f6f58b69ae9a3755a3496c
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2021-09-15 21:44:38 -0500

    Added complexity

--- back/controller/hello.py
+++ back/controller/hello.py
@@ -4,4 +4,7 @@ from rest_framework.response import Response
 
 @api_view(['GET'])
 def hello_world(request):
+    fruits = ["apple", "banana", "cherry"]
+    for x in fruits:
+        print(x)
     return Response({"message": "Hello, Apple!"})
