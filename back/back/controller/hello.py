f1696073498ddcfa47c875ddb259544886a83320
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2021-09-20 23:51:39 -0500

    in develop

--- back/back/controller/hello.py
+++ back/back/controller/hello.py
@@ -1,10 +1,20 @@
+import click
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from wily.__main__ import cli, report;
+from wily.config import load as load_config
 
 
 @api_view(['GET'])
 def hello_world(request):
+
+    #cli()
+    report('cfsm')
+
     fruits = ["apple", "banana", "cherry"]
     for x in fruits:
         print(x)
+    for x in fruits:
+        print(x)
     return Response({"message": "Hello, Apple!"})
+
--- back/back/urls.py
+++ back/back/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from back.controller.hello import hello_world
 
 urlpatterns = [
-    url(r'^hello/$', hello_world),
+    url(r'^hello/$', hello_world)
 ]
