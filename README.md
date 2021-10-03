5ef6ee7b68d2b15fc7847a7c89fe4b2263b28257
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2021-10-03 11:23:33 -0500

    Fix many files

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
+cd code_to_analyze
+
+wily build
+
 pip install -r requirements.txt
 
 py manage.py runserver
--- back/back/services/wily_service.py
+++ back/back/services/wily_service.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from back.dto.wili_dto import WilyDto
@@ -9,35 +10,50 @@ from wily.helper.custom_enums import ReportFormat
 
 
 def generate_report():
-    config: WilyConfig = setWilyConfig()
-    return call_report(config)
+    json = []
+    for path in getPaths():
+        config: WilyConfig = setWilyConfig(path)
+        file = str(path).split('\\')
+        file_name = file[len(file)-1]
+        json.append(call_report(config, file_name))
+    return json
 
 
-def setWilyConfig():
+def setWilyConfig(path: str):
     config: WilyConfig = load_config()
-    path_string = str(Path.cwd())
-    config.path = path_string + '\\back\\controller'
+    full_path = os.path.dirname(path)
+    config.path = str(Path(full_path))
     return config
 
 
-def call_report(config):
+def call_report(config, file_name: str):
     new_output = Path().cwd()
     data = report(
         config=config,
-        path='hello.py',
+        path=file_name,
         metrics=get_default_metrics(config),
-        n=1,
+        n=100,
         output=new_output,
         include_message=True,
         format=ReportFormat.CONSOLE,
         console_format=None,
     )
-    return buildJson(data)
+    return buildWilyDto(data, file_name)
 
 
-def buildJson(data):
-    json = []
+def buildWilyDto(data, file_name):
     for element in data:
-        json.append(
-            WilyDto('hello.py', element[4], element[5], element[6], element[7]))
-    return json
+        wilyDto = WilyDto(
+            file_name, element[4], element[5], element[6], element[7])
+    return wilyDto
+
+
+def getPaths():
+    paths = []
+    absolute_path: str = str(Path().cwd().parent)+"\\code_to_analyze"
+    print(absolute_path)
+    for root, dirs, files in os.walk(absolute_path):
+        for file in files:
+            if file.endswith(".py"):
+                paths.append(os.path.join(root, file))
+    return paths
