3f2d3e6f6c2f2e64a6f3798cba3283bc32233cb9
Cristian Sepulveda <cf.sepulveda@uniandes.edu.co>
2021-11-10 21:01:08 -0500

    restore file

--- back/back/services/wily_service.py
+++ back/back/services/wily_service.py
@@ -1,88 +1,50 @@
 import os
 from pathlib import Path
 
-import click
 from back.dto.wili_dto import WilyDto
 from wily.cache import get_default_metrics
 from wily.commands.report import report
 from wily.commands.build import build as build_wily
 from wily.cache import clean
-from wily.lang import _
-
-
 
 from wily.archivers import resolve_archiver
 from wily.operators import resolve_operators
 
 from wily.config import WilyConfig
 from wily.config import load as load_config
-from wily.config import DEFAULT_ARCHIVER
-from wily.config import DEFAULT_OPERATORS
 from wily.helper.custom_enums import ReportFormat
 
 def generate_report():
     json = []
-    absolute_path: str = str(Path().cwd().parent) + "\\code_to_analyze"
-    build(setWilyConfig(absolute_path))
-
-
     for path in getPaths():
         config: WilyConfig = setWilyConfig(path)
+        #build(config)
         file = str(path).split('\\')
         file_name = file[len(file)-1]
         json.append(call_report(config, file_name))
-    return json
-
 
 def setWilyConfig(path: str):
     config: WilyConfig = load_config()
-    config.path = str(Path(path))
-    config.operators = DEFAULT_OPERATORS
-    config.archiver = DEFAULT_ARCHIVER
+    full_path = os.path.dirname(path)
+    config.path = str(Path(full_path))
     return config
-
-
-def call_report(config, file_name: str):
-    new_output = Path().cwd()
-    data = report(
-        config=config,
-        path=file_name,
-        metrics=get_default_metrics(config),
-        n=100,
-        output=new_output,
-        include_message=True,
-        format=ReportFormat.CONSOLE,
-        console_format=None,
-    )
-    return buildWilyDto(data, file_name)
-
-
-def buildWilyDto(data, file_name):
-    for element in data:
-        wilyDto = WilyDto(
-            file_name, element[4], element[5], element[6], element[7])
-    return wilyDto
 
 
 def getPaths():
     paths = []
     absolute_path: str = str(Path().cwd().parent)+"\\code_to_analyze"
+    print(absolute_path)
     for root, dirs, files in os.walk(absolute_path):
         for file in files:
             if file.endswith(".py"):
                 paths.append(os.path.join(root, file))
     return paths
 
-def build(config, operators):
+def build(config):
+    print(config)
     clean(config)
-
-    try:
-        build_wily(
-            config=config,
-            archiver=resolve_archiver("git"),
-            operators=resolve_operators(operators),
-        )
-    except NameError:
-        print("ocurrio un error")
-        print(NameError)
-        print("ocurrio un error")
+    build_wily(
+        config=config,
+        archiver=resolve_archiver(config.archiver),
+        operators=resolve_operators(config.operators),
+    )
